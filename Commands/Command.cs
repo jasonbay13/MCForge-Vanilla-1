0b71271d14e5b627ed2f5b6bfced804afac6b3dd
hypereddie10 <eddie@gamezgalaxy.com>
2012-01-08 20:13:51 -0500

    opps...

--- Commands/Command.cs
+++ Commands/Command.cs
@@ -117,7 +117,7 @@ namespace MCForge
 			all.Add(new CmdFreeze());
 			all.Add(new CmdGarbage());
             all.Add(new CmdGcaccept());
-			//all.Add(new CmdGifToCin());
+			all.Add(new CmdGifToCin());
 			all.Add(new CmdGive());
 			all.Add(new CmdGlobal());
 			all.Add(new CmdGlobalCLS());
