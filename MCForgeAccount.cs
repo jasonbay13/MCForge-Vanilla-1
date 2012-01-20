32573983f8f6045a67bee0462e73384afa754e83
Techjar <tecknojar@gmail.com>
2012-01-20 06:01:43 -0500

    Anti-error stuffs...

--- MCForgeAccount.cs
+++ MCForgeAccount.cs
@@ -20,38 +20,46 @@ namespace MCForge
 
         public static bool Login(string user, string pass)
         {
-            LoggedIn = false;
-            Server.s.Log("Logging into MCForge.net...");
-            byte[] data = Encoding.ASCII.GetBytes(string.Format("action=do_login&username={0}&password={1}", user, pass));
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.mcforge.net/forums/member.php");
-            request.CookieContainer = new CookieContainer();
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = data.Length;
-            request.Timeout = 15000; // 15 seconds
-
-            using (Stream stream = request.GetRequestStream())
-                stream.Write(data, 0, data.Length);
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            using (Stream stream = response.GetResponseStream())
+            try
             {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    string responseData = reader.ReadToEnd();
-                    if (responseData.IndexOf("You have successfully been logged in.") != -1)
-                    {
-                        Server.s.Log("Successfully logged into MCForge.net!");
-                        Cookies = new CookieContainer();
-                        Cookies.Add(response.Cookies);
-                        LoggedIn = true;
-                    }
-                    else Server.s.Log("Login failed!");
-                }
-            }
+                LoggedIn = false;
+                Server.s.Log("Logging into MCForge.net...");
+                byte[] data = Encoding.ASCII.GetBytes(string.Format("action=do_login&username={0}&password={1}", user, pass));
 
-            return LoggedIn;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.mcforge.net/forums/member.php");
+                request.CookieContainer = new CookieContainer();
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                request.Timeout = 15000; // 15 seconds
+
+                using (Stream stream = request.GetRequestStream())
+                    stream.Write(data, 0, data.Length);
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string responseData = reader.ReadToEnd();
+                        if (responseData.IndexOf("You have successfully been logged in.") != -1)
+                        {
+                            Server.s.Log("Successfully logged into MCForge.net!");
+                            Cookies = new CookieContainer();
+                            Cookies.Add(response.Cookies);
+                            LoggedIn = true;
+                        }
+                        else Server.s.Log("Login failed!");
+                    }
+                }
+
+                return LoggedIn;
+            }
+            catch (Exception ex)
+            {
+                Server.ErrorLog(ex);
+                return false;
+            }
         }
     }
 }
