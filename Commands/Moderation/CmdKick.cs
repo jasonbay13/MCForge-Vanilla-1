d2e4648d9c97fe310a25c0f1477582a2c02814bf
Nicholas Koceja <nerketur@hotmail.com>
2012-04-02 21:54:13 -0400

    Added /who, /kick (again)
    /who is basic, will be updated as more functionality is made.
    /kick can do lots of things, even kick yourself! =D

--- Commands/Moderation/CmdKick.cs
+++ Commands/Moderation/CmdKick.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2011 MCForge
+Copyright 2012 MCForge
 Dual-licensed under the Educational Community License, Version 2.0 and
 the GNU General Public License, Version 3 (the "Licenses"); you may
 not use this file except in compliance with the Licenses. You may
@@ -12,42 +12,92 @@ BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied. See the Licenses for the specific language governing
 permissions and limitations under the Licenses.
 */
-using MCForge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using MCForge.Interface.Command;
 using MCForge.Entity;
 using MCForge.Core;
 
-namespace CommandDll
-{
-    public class CmdKick : ICommand
-    {
-        public string Name { get { return "Kick"; } }
-        public CommandTypes Type { get { return CommandTypes.mod; } }
-        public string Author { get { return "Arrem"; } }
-        public int Version { get { return 1; } }
-        public string CUD { get { return ""; } }
+namespace CommandDll.Moderation {
+	/// <summary>
+	/// Cpommand for kicking someone!
+	/// </summary>
+	public class CmdKick : ICommand {
+		string _Name = "Kick";
+		public string Name { get { return _Name; } }
 
-        public void Use(Player p, string[] args)
-        {
-            if (args.Length == 0) { Help(p); return; }
-            Player who = Player.Find(args[0]); args[0] = "";
-            string message = null;
-            if (args.Length == 1) { message = "Kicked by " + p.USERNAME; }
-            else { foreach (string a in args) { message += a + " "; } }
-            if (Server.devs.Contains(who.USERNAME)) { p.SendMessage("You can't kick a MCForge Developer!"); return; }
-            who.Kick(message);
-            Player.UniversalChat(who.USERNAME + " was kicked by " + p.USERNAME);
-        }
+		CommandTypes _Type = CommandTypes.mod;
+		public CommandTypes Type { get { return _Type; } }
 
-        public void Help(Player p)
-        {
-            p.SendMessage("/kick <player> [message] - kicks a player with an optional message!");
-        }
+		string _Author = "Nerketur";
+		public string Author { get { return _Author; } }
 
-        public void Initialize()
-        {
-            Command.AddReference(this, new string[2] { "kick", "k" });
-        }
-    }
+		int _Version = 1;
+		public int Version { get { return _Version; } }
+
+		string _CUD = "";
+		public string CUD { get { return _CUD; } }
+
+		string[] CommandStrings = new string[1] { "kick" };
+
+		public void Use(Player p, string[] args) {
+			if (args.Length == 0) {
+				//Kick the user
+				p.Kick("Congrats!  You kicked yourself!");
+			} else {
+				List<Player> kickeeList = new List<Player>();
+
+				//Is it an IP or a name?
+				foreach (Player plr in Server.Players.ToArray()) {
+					if (plr.ip == args[0] || plr.username.StartsWith(args[0].ToLower()))
+						kickeeList.Add(plr); //When kicking someone, we don't care for case.
+				}
+				if (kickeeList.Count == 0) {
+					p.SendMessage("Sorry, but the specified player/IP is not online!");
+					return;
+				}
+				String reason;
+				StringBuilder sb = new StringBuilder();
+				foreach (String s in args.Skip(1)) {
+					sb.Append(s);
+					if (s != args[1]) {
+						sb.Append(" ");
+					}
+				}
+				if (sb.Length == 0)
+					reason = "You were kicked by " + p.USERNAME;
+				else
+					reason = sb.ToString();
+				bool kickPlayer = false;
+				foreach (Player kickee in kickeeList) {
+					if (Server.devs.Contains(kickee.USERNAME)) {
+						kickPlayer = true;
+						p.SendMessage("You can't kick the developer " + kickee.USERNAME + "!");
+					} else
+						kickee.Kick(reason);
+				}
+				if (kickPlayer) {
+					String msg = "You tried to kick a developer!  Shame on you!";
+					if (Server.devs.Contains(p.USERNAME))
+						p.SendMessage(msg);
+					else
+						p.Kick(msg);
+				}
+			}
+		}
+
+		public void Help(Player p) {
+			p.SendMessage("/kick [username/ip [message]] - Kicks a username/ip from the server");
+			p.SendMessage("/kick - Kicks YOU from the server.");
+			p.SendMessage("With an IP, it kicks every user with a matching name and/or IP");
+			p.SendMessage("With a username, it kicks every user with a matching beginning name.");
+			p.SendMessage("For example.. /kick ner will kick Ner, ner, Nerk, Nerketur, etc.");
+		}
+
+		public void Initialize() {
+			Command.AddReference(this, CommandStrings);
+		}
+	}
 }
-
