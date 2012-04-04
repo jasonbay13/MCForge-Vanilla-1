74ecc4d283933e1fe6652f4cba33f29d0926b867
cazzar <cayde.dixon@gmail.com>
2012-04-04 15:00:12 +1000

    fixed a bug relating to rank file creation and also added PlayerGroup.Exists() (Group was renamed to PlayerGroup)

--- MCForge 2.0/Groups/PlayerGroup.cs
+++ MCForge 2.0/Groups/PlayerGroup.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+Copyright 2011 MCForge
+Dual-licensed under the Educational Community License, Version 2.0 and
+the GNU General Public License, Version 3 (the "Licenses"); you may
+not use this file except in compliance with the Licenses. You may
+obtain a copy of the Licenses at
+http://www.opensource.org/licenses/ecl2.php
+http://www.gnu.org/licenses/gpl-3.0.html
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licenses are distributed on an "AS IS"
+BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the Licenses for the specific language governing
+permissions and limitations under the Licenses.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -90,7 +105,7 @@ namespace MCForge.Groups
                 Directory.CreateDirectory(Path.GetDirectoryName(file1));
             if (!File.Exists(file1))
             {
-                File.Create(file1);
+                File.Create(file1).Close();
                 Server.Log("[Groups] " + file + " was created", ConsoleColor.DarkGreen, ConsoleColor.Black);
             }
             permission = perm;
@@ -160,8 +175,8 @@ namespace MCForge.Groups
         /// <remarks></remarks>
         public bool LoadGroup()
         {
-            //try
-            //{
+            try
+            {
                 string line;
                 TextReader file = new StreamReader(this.file);
 
@@ -173,11 +188,12 @@ namespace MCForge.Groups
                             players.Add(line.ToLower());
                 }
                 file.Close();
-            //}
-            //catch
-            //{
-            //    return false;
-            //}
+                file.Dispose();
+            }
+            catch
+            {
+                return false;
+            }
             return true;
         }
 
@@ -193,6 +209,22 @@ namespace MCForge.Groups
             new PlayerGroup((byte)Permission.Operator, "Operator", Colors.purple, "ops.txt");
             new PlayerGroup((byte)Permission.SuperOP, "SuperOp", Colors.maroon, "superops.txt");
             new PlayerGroup((byte)Permission.Owner, "Owner", Colors.blue, "owners.txt");
+        }
+
+        /// <summary>
+        /// Checks if the specified group exists
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static PlayerGroup Exists(string name)
+        {
+            foreach (PlayerGroup g in PlayerGroup.groups)
+            {
+                if (g.name.ToLower() == name.ToLower())
+                    return g;
+            }
+            return null;
         }
     }
 }
