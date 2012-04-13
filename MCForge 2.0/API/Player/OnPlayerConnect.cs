09258dcad2459db8a9f64a3c8da0cce1849425d3
hypereddie10 <eddie@gamezgalaxy.com>
2012-04-13 17:46:08 -0400

    Fixed up some typo's

--- MCForge 2.0/API/Player/OnPlayerConnect.cs
+++ MCForge 2.0/API/Player/OnPlayerConnect.cs
@@ -96,7 +96,7 @@ namespace MCForge.API.PlayerEvent
 		/// Unregisters the sxpecified event
 		/// </summary>
 		/// <param name="pe">The event to unregister</param>
-		public static void Unregister(OnPlayerMove pe) {
+		public static void Unregister(OnPlayerConnect pe) {
 			pe.Unregister();
 		}
 		/// <summary>
