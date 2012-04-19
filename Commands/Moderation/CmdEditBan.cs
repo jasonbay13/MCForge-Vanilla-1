1a741921b1ef33d58fe520e6acb68c2348834c30
Sinjai Rakaii <sin@hax.f11.us>
2012-04-18 19:27:38 -0500

    Added /EditBan.

--- Commands/Moderation/CmdEditBan.cs
+++ Commands/Moderation/CmdEditBan.cs
@@ -13,37 +13,45 @@ or implied. See the Licenses for the specific language governing
 permissions and limitations under the Licenses.
 */
 using System;
-using MCForge.Entity;
 using MCForge.Interface.Command;
+using MCForge.Entity;
+using System.Collections.Generic;
 using System.IO;
 
 namespace CommandDll.Moderation
 {
-    class CmdBanInfo : ICommand
+    class CmdEditBan : ICommand
     {
-        public string Name { get { return "BanInfo"; } }
+        public string Name { get { return "EditBan"; } }
         public CommandTypes Type { get { return CommandTypes.mod; } }
         public string Author { get { return "Sinjai"; } }
         public int Version { get { return 1; } }
         public string CUD { get { return ""; } }
         public byte Permission { get { return 0; } }
-        public void Initialize() { Command.AddReference(this, new string[2] { "baninfo", "baninformation" }); }
+        public void Initialize() { Command.AddReference(this, new string[2] { "editban", "banedit" }); }
         public void Use(Player p, string[] args)
         {
             int _ = 0;
+            string _newreason = "";
+            string newreason = _newreason.Substring(args[0].IndexOf(" ") + 1);
             string[] lines = File.ReadAllLines("baninfo.txt");
             if (lines.Length < 1) { p.SendMessage("Could not find ban information for \"" + args[0] + "\"."); return; }
             foreach (string line in lines)
             {
-                string name = line.Split('`')[0];
-                string reason = line.Split('`')[1];
-                string date = line.Split('`')[2];
-                string time = line.Split('`')[3];
-                string banner = line.Split('`')[4];
-                if (args[0] == name)
+                if (line.Split('`')[0] == args[0])
                 {
-                    p.SendMessage(name + " was banned at " + time + " on " + date + " by " + banner + ".");
-                    p.SendMessage("Reason: " + reason);
+                    string date = line.Split('`')[2];
+                    string time = line.Split('`')[3];
+                    string banner = line.Split('`')[4];
+                    List<string> temp = new List<string>();
+                    foreach (string l in lines)
+                    {
+                        if (!l.StartsWith(args[0]))
+                            temp.Add(l);
+                        temp.Add(args[0] + "`" + newreason + "`" + date + "`" + time + "`" + banner);
+                    }
+                    File.WriteAllLines("baninfo.txt", temp.ToArray());
+                    p.SendMessage("Successfully set " + args[0] + "'s ban reason to \"" + newreason + "\".");
                 }
                 else
                 {
@@ -55,7 +63,7 @@ namespace CommandDll.Moderation
         }
         public void Help(Player p)
         {
-            p.SendMessage("/baninfo <player> - See information about <player>'s ban.");
+            p.SendMessage("/editban <player> <reason> - Set <player>'s ban reason to <reason>.");
         }
     }
 }
