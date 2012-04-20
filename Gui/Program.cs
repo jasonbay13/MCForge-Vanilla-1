0d5183e27d752b549392cedf53fdaa502615d068
headdetect <headdetective2@gmail.com>
2012-04-19 22:58:47 -0600

    Guess we don't need this anymore

--- Gui/Program.cs
+++ Gui/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using MCForge.Utilities.Settings;
+using MCForge.Utilities;
+using MCForge.Utils;
+using MCForge;
 
 namespace Gui {
     class Program {
         static void Main(string[] args) {
+            Logger.Init();
+            ServerSettings.Init();
+            ColorUtils.Init();
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //new Thread(new ThreadStart(Server.Init)).Start(); Will be called when the main form is created
+            Application.Run(new frmMain());
         }
     }
 }
