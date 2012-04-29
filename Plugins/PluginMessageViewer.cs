b242909e2e7175f5ebcf40d3b2c8ea3ca964bb97
ninedrafted <none@example.org>
2012-04-29 19:16:00 +0200

    License

--- Plugins/PluginMessageViewer.cs
+++ Plugins/PluginMessageViewer.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2011 MCForge
+Copyright 2012 MCForge
 Dual-licensed under the Educational Community License, Version 2.0 and
 the GNU General Public License, Version 3 (the "Licenses"); you may
 not use this file except in compliance with the Licenses. You may
@@ -11,7 +11,7 @@ software distributed under the Licenses are distributed on an "AS IS"
 BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied. See the Licenses for the specific language governing
 permissions and limitations under the Licenses.
-*/﻿
+*/
 using System;
 using System.Collections.Generic;
 using System.Timers;
