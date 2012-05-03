a56ec4785e39b14036a4616f663c1b863c51a596
ninedrafted <ninedrafted@hotmail.com>
2012-05-03 20:02:22 +0200

    Code reformatted + private->public void SendMessage(Viewer v) + pos->MessagePos
    public void SendMessage(Viewer v)
    Lets cmd/plugins use a created instance of Viewer

--- Plugins/PluginMessageViewer.cs
+++ Plugins/PluginMessageViewer.cs
@@ -20,41 +20,27 @@ using MCForge.Entity;
 using MCForge.API.PlayerEvent;
 using MCForge.Core;
 
-namespace PluginsDLL
-{
-    public class PluginMessageViewer : IPluginMessageViewer
-    {
+namespace PluginsDLL {
+    public class PluginMessageViewer : IPluginMessageViewer {
         List<Viewer> viewing = new List<Viewer>();
-        public void ShowMessage(Player p, string message)
-        {
+        public void ShowMessage(Player p, string message) {
             if (_isUnloading) return;
             int i = viewing.FindIndex(v => { return v.p.id == p.id; });
-            if (i >= 0)
-            {
+            if (i >= 0) {
                 viewing[i].message = prepare(message);
-                viewing[i].pos = 0;
+                viewing[i].MessagePos = 0;
             }
-            else
-            {
+            else {
                 viewing.Add(new Viewer(p, message, 0));
                 viewing[viewing.Count - 1].NeedsUpdate += new Viewer.update(SendMessage);
                 OnPlayerDisconnect.Register(OnDisconnect, p);
                 viewing[viewing.Count - 1].ForceUpdate();
             }
         }
-        private void SendEmptyMessage(Viewer v)
-        {
-            packet pa = new packet();
-            pa.Add(packet.types.Message);
-            pa.Add(v.p.id);
-            pa.Add(".",64);
-        }
-        private void SendMessage(Viewer v)
-        {
-            for (int i = 0; i < totalHeight-messageHeight; i++)
+        public void SendMessage(Viewer v) {
+            for (int i = 0; i < totalHeight - messageHeight; i++)
                 v.p.SendMessage(" ");
-            for (int i = v.pos; i < v.pos + messageHeight; i++)
-            {
+            for (int i = v.MessagePos; i < v.MessagePos + messageHeight; i++) {
                 if (i < v.message.Length && i >= 0)
                     if (v.message[i] != "")
                         v.p.SendMessage(v.message[i]);
@@ -68,17 +54,14 @@ namespace PluginsDLL
         private static int totalHeight = 20;
         private static char col = '%';
         private static double refreshTime = 9500;
-        private static int calcLength(string part)
-        {
+        private static int calcLength(string part) {
             return part.Length; // -2 * part.Count(c => { return c == col; });
         }
-        private static string getMaxString(string line, int max)
-        {
+        private static string getMaxString(string line, int max) {
             int o = line.Length - line.TrimStart().Length;
             int vlen = 0;
             int i = o;
-            for (; i + max < line.Length && vlen < max; i++)
-            {
+            for (; i + max < line.Length && vlen < max; i++) {
                 vlen = calcLength(line.Substring(o, max + i));
             }
             if (o + max + i >= line.Length) return line;
@@ -86,17 +69,14 @@ namespace PluginsDLL
             if (t >= 0) i -= i + max - t;
             return line.Substring(0, (o + max + i < line.Length) ? o + max + i : line.Length).TrimEnd();
         }
-        private static string[] prepare(string text)
-        {
+        private static string[] prepare(string text) {
             //Todo: Colors for multiple lines
             List<string> ret = new List<string>();
             string[] lines = text.Split('\n');
-            foreach (string l in lines)
-            {
+            foreach (string l in lines) {
                 int i = 0;
                 string t = getMaxString(l.Substring(i), width);
-                while (i + t.Length < l.Length)
-                {
+                while (i + t.Length < l.Length) {
                     ret.Add(t.TrimStart());
                     i += t.Length;
                     t = getMaxString(l.Substring(i), width);
@@ -105,8 +85,7 @@ namespace PluginsDLL
             }
             return ret.ToArray();
         }
-        private int indexOfPlayer(Player p)
-        {
+        private int indexOfPlayer(Player p) {
             return viewing.FindIndex(v => { return v.p.Username == p.Username; });
         }
 
@@ -115,38 +94,31 @@ namespace PluginsDLL
         public int Version { get { return 1; } }
         public string CUD { get { return ""; } }
 
-        public class Viewer
-        {
-            public Viewer(Player p, string message, int pos) : this(p, prepare(message), pos) { }
-            private Viewer(Player p, string[] message, int pos)
-            {
+        public class Viewer {
+            public Viewer(Player p, string message, int messagePos) : this(p, prepare(message), messagePos) { }
+            private Viewer(Player p, string[] message, int messagePos) {
                 this.p = p;
                 this.message = message;
-                this.pos = pos;
+                this.MessagePos = messagePos;
                 t = new Timer(refreshTime);
                 t.Elapsed += new ElapsedEventHandler(t_Elapsed);
             }
-            void t_Elapsed(object sender, ElapsedEventArgs e)
-            {
-                if (NeedsUpdate != null)
-                {
+            void t_Elapsed(object sender, ElapsedEventArgs e) {
+                if (NeedsUpdate != null) {
                     NeedsUpdate(this);
                 }
             }
             public Player p;
             public string[] message;
-            public int pos;
-            public void ForceUpdate()
-            {
-                if (NeedsUpdate != null)
-                {
+            public int MessagePos;
+            public void ForceUpdate() {
+                if (NeedsUpdate != null) {
                     t.Stop();
                     NeedsUpdate(this);
                     t.Start();
                 }
             }
-            public void Stop()
-            {
+            public void Stop() {
                 t.Stop();
                 t.Close();
             }
@@ -154,78 +126,60 @@ namespace PluginsDLL
             public delegate void update(Viewer v);
             Timer t;
         }
-        private void setMessagePos(Player p, int pos)
-        {
+        private void setMessagePos(Player p, int pos) {
             int index = indexOfPlayer(p);
-            if (index >= 0)
-            {
+            if (index >= 0) {
                 if (pos >= minPos(viewing[index]) && pos <= maxPos(viewing[index]))
-                    viewing[index].pos = pos;
-                else if (pos < minPos(viewing[index])) viewing[index].pos = minPos(viewing[index]);
-                else viewing[index].pos = maxPos(viewing[index]);
+                    viewing[index].MessagePos = pos;
+                else if (pos < minPos(viewing[index])) viewing[index].MessagePos = minPos(viewing[index]);
+                else viewing[index].MessagePos = maxPos(viewing[index]);
                 viewing[index].ForceUpdate();
             }
         }
-        private int maxPos(Viewer v)
-        {
+        private int maxPos(Viewer v) {
             return v.message.Length - 1;
         }
-        private int minPos(Viewer v)
-        {
+        private int minPos(Viewer v) {
             return 1 - messageHeight;
         }
-        private void moveMessagePos(Player p, int move)
-        {
+        private void moveMessagePos(Player p, int move) {
             int index = indexOfPlayer(p);
-            if (index >= 0)
-            {
-                if (viewing[index].pos + move >= minPos(viewing[index]) && viewing[index].pos + move <= maxPos(viewing[index]))
-                    viewing[index].pos = viewing[index].pos + move;
-                else if (viewing[index].pos + move < minPos(viewing[index]))
-                    viewing[index].pos = minPos(viewing[index]);
-                else viewing[index].pos = maxPos(viewing[index]);
+            if (index >= 0) {
+                if (viewing[index].MessagePos + move >= minPos(viewing[index]) && viewing[index].MessagePos + move <= maxPos(viewing[index]))
+                    viewing[index].MessagePos = viewing[index].MessagePos + move;
+                else if (viewing[index].MessagePos + move < minPos(viewing[index]))
+                    viewing[index].MessagePos = minPos(viewing[index]);
+                else viewing[index].MessagePos = maxPos(viewing[index]);
                 viewing[index].ForceUpdate();
             }
         }
-        public void ShowNextPage(Player p)
-        {
+        public void ShowNextPage(Player p) {
             int index = indexOfPlayer(p);
-            if (index >= 0)
-            {
-                if (viewing[index].pos < 0) setMessagePos(viewing[index].p, 0);
-                else moveMessagePos(p, (viewing[index].pos / messageHeight + 1) * messageHeight);
+            if (index >= 0) {
+                if (viewing[index].MessagePos < 0) setMessagePos(viewing[index].p, 0);
+                else moveMessagePos(p, (viewing[index].MessagePos / messageHeight + 1) * messageHeight);
             }
         }
-
-        public void ShowPage(Player p, int page)
-        {
+        public void ShowPage(Player p, int page) {
             setMessagePos(p, (page - 1) * messageHeight);
         }
 
-        public void ShowNextLine(Player p)
-        {
+        public void ShowNextLine(Player p) {
             moveMessagePos(p, 1);
         }
-
-        public void ShowPreviousPage(Player p)
-        {
+        public void ShowPreviousPage(Player p) {
             int index = indexOfPlayer(p);
-            if (index >= 0)
-            {
-                moveMessagePos(p, (viewing[index].pos / messageHeight - 2) * messageHeight);
+            if (index >= 0) {
+                moveMessagePos(p, (viewing[index].MessagePos / messageHeight - 2) * messageHeight);
             }
         }
-
-        public void ShowPreviousLine(Player p)
-        {
+        public void ShowPreviousLine(Player p) {
             moveMessagePos(p, -1);
         }
-        private void SendEnd(Player p)
-        {
+        private void SendEnd(Player p) {
             int index = indexOfPlayer(p);
             int i = viewing[index].message.Length - totalHeight;
-            for (i = (i < 0) ? 0 : i; i < viewing[index].message.Length; i++)
-            {
+            for (i = (i < 0) ? 0 : i; i < viewing[index].message.Length; i++) {
                 p.SendMessage(viewing[index].message[i]);
             }
         }
@@ -234,10 +188,9 @@ namespace PluginsDLL
         /// </summary>
         /// <param name="p">The player</param>
         /// <param name="text">The Text</param>
-        public void AppendText(Player p, string text)
-        {
+        public void AppendText(Player p, string text) {
             int index = indexOfPlayer(p);
-            bool update = viewing[index].pos + messageHeight > viewing[index].message.Length;
+            bool update = viewing[index].MessagePos + messageHeight > viewing[index].message.Length;
             string[] append = prepare(text);
             string[] message = new string[append.Length + viewing[index].message.Length];
             viewing[index].message.CopyTo(message, 0);
@@ -245,34 +198,27 @@ namespace PluginsDLL
             viewing[index].message = message;
             if (update) viewing[index].ForceUpdate();
         }
-
-
-        public void Stop(Player p)
-        {
+        public void Stop(Player p) {
             int i = indexOfPlayer(p);
-            if (i >= 0)
-            {
+            if (i >= 0) {
                 SendEnd(p);
                 viewing[i].Stop();
                 viewing.RemoveAt(i);
                 //OnPlayerDisconnect.Unregister(OnDisconnect, p); //only one has to be unregistered!
             }
         }
-        public void OnDisconnect(OnPlayerDisconnect eventargs)
-        {
+        public void OnDisconnect(OnPlayerDisconnect eventargs) {
             Stop(eventargs.Player);
         }
         bool _isUnloading = false;
-        public void OnUnload()
-        {
+        public void OnUnload() {
             _isUnloading = true;
-            for (int i = viewing.Count-1; i >=0; i--)
-            {
+            for (int i = viewing.Count - 1; i >= 0; i--) {
                 Stop(viewing[i].p);
             }
         }
         public void OnLoad(string[] args) {
-            
+
         }
     }
 }
