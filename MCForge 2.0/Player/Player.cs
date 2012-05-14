6f12c99e7f557b0b9fe9dc3cd3f312d1f36d2ead
David Diaz (Snowl) <d@viddiaz.com>
2012-05-14 16:17:13 +1000

    Moved bots into /Robot

--- MCForge 2.0/Player/Player.cs
+++ MCForge 2.0/Player/Player.cs
@@ -494,6 +494,28 @@ namespace MCForge.Entity {
             //TODO Update form list
         }
 
+        /// <summary>
+        /// Rainbows some text
+        /// </summary>
+        /// <param name="strin">The string input</param>
+        /// <returns>Outputs some colorful RAIIIIIINBBBBBBOWWWWWWW text</returns>
+        public string Rainbow(string strin)
+        {
+            string rainbowString = "4c6eb3912ad5";
+            string rainbow = "";
+            int loop = 0;
+
+            for (int i = 0; i < strin.Length; i++)
+            {
+                rainbow = rainbow + "&" + rainbowString[loop].ToString() + strin[i].ToString();
+                if (loop == rainbowString.Length - 1)
+                    loop = 0;
+                else
+                    loop++;
+            }
+            return rainbow;
+        }
+
         #region Verification Stuffs
         private  void CheckMultipleConnections() {
 
