3510840ff9ebe3ab655034af6cc899c15c934c31
David Diaz (Snowl) <d@viddiaz.com>
2012-05-19 15:00:05 +1000

    Statically linked dll's are bad :( Fixed SQLite

--- MCForge 2.0/API/Events/Robot/OnBotMove.cs
+++ MCForge 2.0/API/Events/Robot/OnBotMove.cs
@@ -6,7 +6,6 @@ using MCForge.Entity;
 using MCForge.Robot;
 using MCForge.Core;
 using MCForge.Utils;
-
 namespace MCForge.API.Events.Robot
 {
     public class MoveBotEvent : Event<Bot, MoveEventArgs>
