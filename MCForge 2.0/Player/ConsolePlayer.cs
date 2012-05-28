68f78749ac54a5aca62683d09018280cfbaf4545
ninedrafted <ninedrafted@hotmail.com>
2012-05-28 15:31:32 +0200

    invokable events from commandline

--- MCForge 2.0/Player/ConsolePlayer.cs
+++ MCForge 2.0/Player/ConsolePlayer.cs
@@ -31,8 +31,6 @@ namespace MCForge.Entity {
             base.OnPlayerReceiveUnknownPacket.OnUnregister += new EventHandler<EventRegisterArgs>(OnRegister);
 
         }
-
-
         void OnRegister(object sender, EventRegisterArgs args) {
             if (args.Registering) {
                 IO.WriteLine(args.Method.Target.GetType().Name + " registered to receive " + args.ArgsType.Name + " from " + args.SenderType.Name);
@@ -44,12 +42,92 @@ namespace MCForge.Entity {
         }
         void AskForEventArgs(EventRegisterArgs args) {
             if (typeof(Player) == args.SenderType) {
-                IO.WriteLine("Properties: ");
-                PropertyInfo[] props = args.ArgsType.GetProperties();
-                for (int i = 0; i < props.Length; i++) {
-                    IO.WriteLine(props[i].Name + " (" + props[i].PropertyType.Name + ")");
+                object oargs = createInstance(selectConstructer(args.ArgsType));
+                args.Method.Method.Invoke(args.Method.Target, new object[] {this, oargs });
+            }
+        }
+        ConstructorInfo selectConstructer(Type t) {
+            ConstructorInfo[] ci = t.GetConstructors();
+            for (int i = 0; i < ci.Length; i++) {
+                IO.WriteLine(i + ": " + ci[i]);
+            }
+            if (ci.Length == 1) return ci[0];
+            IO.WriteLine("Select a constructer for " + t.Name);
+            string read = IO.ReadLine();
+            int c = 0;
+            try {
+                c = Int32.Parse(read);
+            }
+            catch { return null; }
+            if (c < ci.Length && c >= 0)
+                return ci[c];
+            return null;
+        }
+        object createInstance(ConstructorInfo c) {
+            ParameterInfo[] pi=c.GetParameters();
+            object[] para = new object[pi.Length];
+            bool singleLine = true;
+            for (int i = 0; i < pi.Length; i++) {
+                if (!pi[i].ParameterType.IsValueType) singleLine = false;
+            }
+            if (singleLine) {
+                string types = "";
+                for (int i = 0; i < pi.Length; i++) {
+                    types += "[ " + pi[i].Name + " as " + pi[i].ParameterType + " ]";
+                }
+                IO.WriteLine("Enter all values splitted by a whitespace\n" + types);
+                string val = IO.ReadLine();
+                string[] vals = val.Split(' ');
+                if (vals.Length >= para.Length) {
+                    for (int i = 0; i < para.Length; i++) {
+                        para[i] = createValueType(pi[i], vals[i]);
+                    }
+                }
+                else singleLine = false;
+            }
+            if (!singleLine) {
+                for (int i = 0; i < pi.Length; i++) {
+                    if (pi[i].ParameterType.IsValueType || pi[i].ParameterType == typeof(string)) {
+                        IO.WriteLine("Enter value for [ " + pi[i].Name + " as " + pi[i].ParameterType.Name+" ]");
+                        string val = IO.ReadLine();
+                        para[i] = createValueType(pi[i], val);
+                    }
+                    else {
+                        para[i] = createInstance(selectConstructer(pi[i].ParameterType));
+                    }
                 }
             }
+            return Activator.CreateInstance(c.ReflectedType,para,null);
+        }
+        object createValueType(ParameterInfo pi, string val) {
+            if (pi.ParameterType.IsAssignableFrom(typeof(string))) {
+                return (object)val;
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(int))) {
+                return (object)int.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(short))) {
+                return (object)short.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(long))) {
+                return (object)long.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(byte))) {
+                return (object)byte.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(ushort))) {
+                return (object)ushort.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(uint))) {
+                return (object)uint.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(ulong))) {
+                return (object)ulong.Parse(val);
+            }
+            else if (pi.ParameterType.IsAssignableFrom(typeof(sbyte))) {
+                return (object)sbyte.Parse(val);
+            }
+            return null;
         }
         private IIOProvider IO;
 
