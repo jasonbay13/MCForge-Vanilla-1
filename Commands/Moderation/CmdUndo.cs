c59c9492bee3c0fd4e655cad321f1bf1f5340847
headdetect <headdetective2@gmail.com>
2012-06-12 12:07:09 -0600

    full undo

--- Commands/Moderation/CmdUndo.cs
+++ Commands/Moderation/CmdUndo.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MCForge.Interface.Command;
 using MCForge.Entity;
 using MCForge.Groups;
+using MCForge.Core;
 
 namespace MCForge.Commands.Moderation {
     public class CmdUndo : ICommand {
@@ -37,25 +38,42 @@ namespace MCForge.Commands.Moderation {
         public void Use(Player p, string[] args) {
 
             int _time = 30;
-            Player who = null;
+            Player who = p;
 
-            if (args.Length < 1) {
-                Help(p);
-                return;
-            }
 
+            //undo <seconds>
             if (args.Length == 1) {
                 try {
                     _time = int.Parse(args[0]);
-                    who = p;
                 }
                 catch {
                     p.SendMessage("That not a number silly head");
                     return;
                 }
-
             }
 
+
+            if (args.Length == 2) {
+                who = Player.Find(args[0]);
+
+                if (who == null) {
+                    p.SendMessage("Player doesn't exist");
+                    return;
+                }
+
+                if (args[1].ToLower() == "all") {
+                    
+                    _time = int.MaxValue;
+                    return;
+                }
+
+                try {
+                    _time = int.Parse(args[1]);
+                }
+                catch {
+                    p.SendMessage("That is not a valid numba");
+                }
+            }
 
             if (p.Group.Permission < (byte)PermissionLevel.Operator) {
                 if (who != p) {
@@ -63,35 +81,38 @@ namespace MCForge.Commands.Moderation {
                     return;
                 }
             }
-
-
+            Undo(who, _time);
+            p.SendMessage(Server.DefaultColor + "Undid " +who.Color + who.Username + Server.DefaultColor + " for &c" + _time + Server.DefaultColor + " seconds");
         }
 
         void Undo(Player p, int time = 30) {
             if (p == null || p.BlockChanges.Count < 0)
                 return;
 
-            int timeToLook = p.BlockChanges[p.BlockChanges.Count - 1].Time.Second - time;
+            long timeToLook = p.BlockChanges[p.BlockChanges.Count - 1].Time.Second - time;
 
             if (timeToLook < 0)
                 timeToLook = p.BlockChanges[0].Time.Second;
+            else if (timeToLook > int.MaxValue)
+                timeToLook = int.MaxValue;
 
-            for (int i = 0; i < p.BlockChanges.Count ; i++) {
+
+            for (int i = 0; i < p.BlockChanges.Count; i++) {
                 var bChange = p.BlockChanges[i];
 
                 if (bChange.Time.Second >= timeToLook) {
                     for (int j = p.BlockChanges.Count; j > i; j--) {
                         p.Level.BlockChange(bChange.Position, bChange.BlockFrom);
                     }
-                    //return;
                 }
             }
         }
 
         public void Help(Player p) {
-            p.SendMessage("/undo <player> <seconds> - Undoes the block changes for <player> in the past <seconds>");
-            p.SendMessage("/undo <player> all - Undoes as much as it can.");
-            p.SendMessage("/undo <seconds> - Unodes the block changes for yourself in the past <seconds>");
+            p.SendMessage(Server.DefaultColor +"/undo <player> <seconds> - Undoes the block changes for <player> in the past <seconds>");
+            p.SendMessage(Server.DefaultColor +"/undo <player> all - Undoes as much as it can.");
+            p.SendMessage(Server.DefaultColor +"/undo <seconds> - Unodes the block changes for yourself in the past <seconds>");
+            p.SendMessage(Server.DefaultColor + "/undo - Undoes the block changes for yourself in the past &c30 " + Server.DefaultColor + " seconds");
         }
 
         public void Initialize() {
