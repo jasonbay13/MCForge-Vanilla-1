aa720f60908dcdcd9b70e0080e572761feb6b3a0
Eddie Penta <eddie@gamezgalaxy.com>
2012-06-24 16:39:01 -0400

    Basic StyleCop rules
    These will be changed as we go

---
+++ MCForge 2.0/System/Updater.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eddie
+ * Date: 6/24/2012
+ * Time: 4:07 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace MCForge.System
+{
+	/// <summary>
+	/// Description of Updater.
+	/// </summary>
+	public class Updater
+	{
+		public Updater()
+		{
+		}
+	}
+}
