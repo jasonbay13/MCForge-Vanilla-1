1239890e82503544e0f697af4209df0c0316292b
cazzar <cayde.dixon@gmail.com>
2012-07-02 14:31:34 +1000

    A newer console
    Fixes contained within the commit are things like the splitting of the
    line when the server prints a new line to the console

--- Console/Program.cs
+++ Console/Program.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Threading;
+using System.Linq;
 using MCForge.Interface;
 using MCForge.Utils;
 using MCForge.Utils.Settings;
@@ -25,17 +26,41 @@ using MCForge.Interface.Command;
 
 namespace MCForge.Core {
     static class Program {
-        private static void Logger_OnRecieveErrorLog(object sender, LogEventArgs e) {
+        /// <summary>
+        /// Handles the OnRecieveErrorLog event of the Logger control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MCForge.Utils.LogEventArgs"/> instance containing the event data.</param>
+        /// <remarks></remarks>
+        private static void Logger_OnRecieveErrorLog(object sender, LogEventArgs e)
+        {
+            ClearCurrentConsoleLine();
             var prevColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("[Error] " + e.Message);
+            WriteLine("[Error] " + e.Message);
             Console.ForegroundColor = prevColor;
         }
+
         /// <summary>
-        /// The main entry point for the application.
+        /// Handles the OnRecieveLog event of the Logger control.
         /// </summary>
-        [STAThread]
-        static void Main(string[] args) {
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="message">The <see cref="MCForge.Utils.LogEventArgs"/> instance containing the event data.</param>
+        /// <remarks></remarks>
+        private static void Logger_OnRecieveLog(object sender, LogEventArgs message)
+        {
+            ClearCurrentConsoleLine();
+            WriteLine(message.Message);
+            WriteInputLine("> ", input);
+        }
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+        }
+        static string input = "";
+
+        static void Main(string[] args)
+        {
+            cp = new ConsolePlayer(cio);
             ServerSettings.Init();
             bool checker = CheckArgs(args);
             Console.Title = ServerSettings.GetSetting("ServerName") + " - MCForge 6"; //Don't know what MCForge version we are using yet.
@@ -43,160 +68,144 @@ namespace MCForge.Core {
                 new Thread(new ThreadStart(Server.Init)).Start();
             else
                 Logger.Log("Aborting Setup..", LogType.Critical);
-            Logger.OnRecieveLog += new EventHandler<LogEventArgs>(Server.OnLog);
+
+            //declare the Hooks
+            //Error Logging
             Logger.OnRecieveErrorLog += new EventHandler<LogEventArgs>(Logger_OnRecieveErrorLog);
-            cp = new ConsolePlayer(cio);
-            while (true) {
-                string input = Console.ReadLine();
-                if (input == null) { Server.Stop(); return; }
-                if (input.Trim().StartsWith("/")) {
-                    if (input.Trim().Length > 1) {
-                        string name = input.Trim().Substring(1);
-                        ICommand c = Command.Find(name);
-                        if (c != null) {
-                            int i = input.Trim().IndexOf(" ");
-                            if (i > 0) {
-                                string[] cargs = input.Trim().Substring(i + 1).Split(' ');
-                                c.Use(cp, cargs);
-                            }
-                            else {
-                                c.Use(cp, new string[0]);
-                            }
+            //Normal Logs
+            Logger.OnRecieveLog += new EventHandler<LogEventArgs>(Logger_OnRecieveLog);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            ConsoleKeyInfo keyInfo;
+            if (Console.CursorLeft < 3)
+                WriteInputLine();
+            while ((keyInfo = Console.ReadKey()) != null)
+            {
+                char key = keyInfo.KeyChar;
+                //handles escape
+                if (keyInfo.Key == ConsoleKey.Escape) continue;
+                // Ignore if Alt or Ctrl is pressed
+                if ((keyInfo.Modifiers & ConsoleModifiers.Alt) == ConsoleModifiers.Alt)
+                    continue;
+                if ((keyInfo.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
+                    continue;
+                // Ignore if KeyChar value is \u0000.
+                if (keyInfo.KeyChar == '\u0000') continue;
+                // Ignore tab key.
+                if (keyInfo.Key == ConsoleKey.Tab) continue;
+                // handle function keys
+                if (keyInfo.Key >= ConsoleKey.F1 && keyInfo.Key <= ConsoleKey.F24) continue;
+
+                if (keyInfo.Key != ConsoleKey.Enter)
+                {
+                    if (keyInfo.Key == ConsoleKey.Backspace && input.Length > 0)
+                    {
+                        ClearConsoleLine(Console.CursorTop);
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        if (!(input.Length < 1))
+                        {
+                            input = input.Remove(input.Length - 1, 1);
+                            WriteInputLine("> " + input);
                         }
                     }
+                    else if (Char.IsLetterOrDigit(key) || Char.IsPunctuation(key) || Char.IsSymbol(key) || key == ' ')
+                        input += key.ToString();
+                    continue;
                 }
-                if (input.ToLower() == "!stop") {
-                    Console.Write("Would you like to save all? [y/n]:");
-                    if (Console.ReadLine().ToLower().StartsWith("y")) {
-                        Server.SaveAll();
-                        Server.Stop();
+                else
+                {
+                    if (String.IsNullOrWhiteSpace(input))
+                    {
+                        WriteInputLine();
+                        continue;
                     }
-                    else {
-                        Server.Stop();
-                    }
-                    return;
+                    WriteLine("");
+                    Handle(input);
+                    input = "";
+                    WriteInputLine();
                 }
-                else if (input.ToLower() == "!copyurl") {
-                    System.Windows.Forms.Clipboard.SetDataObject(Server.URL, true);
-                }
-                else if (input.ToLower().Split(' ')[0] == "!packets") {
-                    string[] cargs = input.ToLower().Split(' ');
-                    if (cargs.Length == 1) {
-                        MCForge.Entity.Player.OnAllPlayersReceivePacket.Important += new API.Events.PacketEvent.EventHandler(OnPacket);
-                        MCForge.Entity.Player.OnAllPlayersSendPacket.Important += new API.Events.PacketEvent.EventHandler(OnPacket);
-                        MCForge.Entity.Player.OnAllPlayersReceiveUnknownPacket.Important += new API.Events.PacketEvent.EventHandler(OnPacket);
-                    }
-                    if (cargs.Length >= 2) {
-                        switch (cargs[1]) {
-                            case "stop":
-                                MCForge.Entity.Player.OnAllPlayersReceivePacket.Important -= new API.Events.PacketEvent.EventHandler(OnPacket);
-                                MCForge.Entity.Player.OnAllPlayersSendPacket.Important -= new API.Events.PacketEvent.EventHandler(OnPacket);
-                                MCForge.Entity.Player.OnAllPlayersReceiveUnknownPacket.Important -= new API.Events.PacketEvent.EventHandler(OnPacket);
-                                break;
-                            case "hide":
-                                if (cargs.Length >= 3) {
-                                    byte t;
-                                    if (Byte.TryParse(cargs[2], out t)) {
-                                        if (!hidePackets.Contains(t)) {
-                                            hidePackets.Add(t);
-                                        }
-                                    }
-                                }
-                                break;
-                            case "unhide":
-                                if (cargs.Length >= 3) {
-                                    byte t;
-                                    if (Byte.TryParse(cargs[2], out t)) {
-                                        hidePackets.Remove(t);
-                                    }
-                                }
-                                break;
-                            case "cancel":
-                                if (cargs.Length >= 3) {
-                                    byte t;
-                                    if (Byte.TryParse(cargs[2], out t)) {
-                                        if (!cancelPackets.Contains(t)) {
-                                            cancelPackets.Add(t);
-                                        }
-                                    }
-                                }
-                                break;
-                            case "allow":
-                                if (cargs.Length >= 3) {
-                                    byte t;
-                                    if (Byte.TryParse(cargs[2], out t)) {
-                                        cancelPackets.Remove(t);
-                                    }
-                                }
-                                break;
-                            case "xth":
-                                if (cargs.Length >= 4) {
-                                    byte t;
-                                    if (Byte.TryParse(cargs[2], out t)) {
-                                        int x;
-                                        if (Int32.TryParse(cargs[3], out x)) {
-                                            if (reducePackets.ContainsKey(t)) {
-                                                reducePackets[t] = x;
-                                            }
-                                            else reducePackets.Add(t, x);
-                                            if (xthPackets.ContainsKey(t)) {
-                                                xthPackets[t] = x; //The first packet is allowed
-                                            }
-                                            else xthPackets.Add(t, x);
-                                        }
-                                    }
-                                }
-                                break;
-                            default:
-                                Console.WriteLine("++ To hide/unhide ++\nhide [type number]\n unhide [type number]\n\n++ To cancel/allow ++\ncancel [type number]\nallow [type number]\n\n++ To handle only xth packet ++\nxth [type number] [xth packet number]");
-                                break;
-                        }
-                    }
-                }
-                //You can use this to talk to the players, someone is probably going to improve this.
-                else {
-                    Player.UniversalChat(Colors.white + "[Console] " + Server.DefaultColor + input);
-                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
-                    Logger.Log("[Console] " + input, Color.Yellow, Color.Black, LogType.Normal);
-                }
-
             }
         }
-        private static ConsolePlayer cp;
-        private static CommandIO cio = new CommandIO();
-        private static List<byte> hidePackets = new List<byte>();
-        private static List<byte> cancelPackets = new List<byte>();
-        private static Dictionary<byte, int> reducePackets = new Dictionary<byte, int>();
-        private static Dictionary<byte, int> xthPackets = new Dictionary<byte, int>();
-        private static void OnPacket(MCForge.Entity.Player sender, API.Events.PacketEventArgs args) {
-            if (cancelPackets.Contains((byte)args.Type)) {
-                args.Cancel();
-                return;
-            }
-            if (reducePackets.ContainsKey((byte)args.Type)) {
-                if (xthPackets[(byte)args.Type] % reducePackets[(byte)args.Type] != 0) {
-                    xthPackets[(byte)args.Type]++;
-                    args.Cancel();
-                    return;
+
+        private static void Handle(string input)
+        {
+            //check if it is a command
+            if (input.StartsWith("/"))
+            {
+                ICommand cmd = null;
+
+                string[] commandSplit = input.Remove(0, 1).Split(' ');
+                string[] args = commandSplit.Where((val, index) => index != 0).ToArray();
+                cmd = Command.Find(commandSplit[0]);
+
+                if (cmd == null)
+                {
+                    WriteLine("Command not found!");
+                    return; // cannot run the command
                 }
-                else {
-                    xthPackets[(byte)args.Type] = 1;
-                }
+                cmd.Use(null, args);
+                Logger.Log("CONSOLE used: /" + commandSplit[0]);
             }
-            if (hidePackets.Contains((byte)args.Type)) return;
-            Logger.Log(((args.Incoming) ? "Incoming" : "Outgoing") + " Packet " + (Packet.Types)(byte)args.Type + "(" + (byte)args.Type + ") for player " + sender.Username, ((args.Incoming) ? Color.Green : Color.Red), Color.Black);
+            else
+            {
+                Player.UniversalChat(Colors.white + "[Console] " + Server.DefaultColor + input);
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                Logger.Log("[Console] " + input, Color.Yellow, Color.Black, LogType.Normal);
+            }
+        }
+        #region Console Functions
+        private static void ClearConsoleLine(int line)
+        {
+            string l = "";
+            for (int i = 0; i <= Console.BufferWidth; i++)
+                l += " ";
+
+            int[] oldpos = { Console.CursorLeft, Console.CursorTop };
+            Console.SetCursorPosition(0, line);
+            Console.Write(l);
+            Console.SetCursorPosition(oldpos[0], oldpos[1]);
+        }
+        private static void ClearCurrentConsoleLine()
+        {
+            string l = "";
+            for (int i = 0; i <= Console.BufferWidth; i++)
+                l += " ";
+
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(l);
+            Console.SetCursorPosition(0, Console.CursorTop -1);
         }
 
+        static void Write(string text)
+        {
+            ClearConsoleLine(Console.CursorTop + 1);
+            Console.Write(text);
+        }
+        public static void WriteLine(string s)
+        {
+            ClearConsoleLine(Console.CursorTop + 1);
+            Console.WriteLine(s);
+        }
+        static void WriteInputLine(string inputline = "> ", string input = "")
+        {
+            if (Console.CursorLeft > 0)
+                Console.WriteLine();
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(inputline + input);
+            Console.SetCursorPosition((inputline + input).Length, Console.CursorTop);
+        }
         /// <summary>
         /// Check the args that were passed on program startup
         /// </summary>
         /// <param name="args">The args</param>
         /// <returns>If returns false, run normal setup. If returns true, cancel normal setup. In this case something has already started the server.</returns>
-        static bool CheckArgs(string[] args) {
+        static bool CheckArgs(string[] args)
+        {
             if (args.Length == 0)
                 return false;
             string name = args[0];
-            switch (name) {
+            switch (name)
+            {
                 case "load-plugin":
                     if (args.Length == 1)
                         return false;
@@ -214,22 +223,29 @@ namespace MCForge.Core {
             }
             return args[args.Length - 1] == "abort-setup";
         }
-    }
-    class CommandIO : IIOProvider {
-        public string ReadLine() {
-            if (Console.CursorLeft != 0) Console.WriteLine();
-            Console.Write("CIO input: ");
-            return Console.ReadLine();
-        }
+        #endregion
 
-        public void WriteLine(string line) {
-            if (Console.CursorLeft != 0) Console.WriteLine();
-            Console.WriteLine("CIO output: " + line);
-        }
-
-        public void WriteLine(string line, string replyChannel)
+        private static ConsolePlayer cp;
+        private static CommandIO cio = new CommandIO();
+        class CommandIO : IIOProvider
         {
-            
+            public string ReadLine()
+            {
+                if (Console.CursorLeft != 0) Console.WriteLine();
+                Console.Write("CIO input: ");
+                return Console.ReadLine();
+            }
+
+            public void WriteLine(string line)
+            {
+                if (Console.CursorLeft != 0) Console.WriteLine();
+                Console.WriteLine("CIO output: " + line);
+            }
+
+            public void WriteLine(string line, string replyChannel)
+            {
+
+            }
         }
     }
 }
