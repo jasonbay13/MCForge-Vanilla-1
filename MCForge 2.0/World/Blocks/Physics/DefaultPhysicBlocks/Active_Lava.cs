d3a0fb520501d88b5353d51a45c7c95cb4ccf55d
GameMakerGm <gamemakergm@safaree.co.cc>
2012-07-06 20:13:53 -0600

    For Gamez

--- MCForge 2.0/World/Blocks/Physics/DefaultPhysicBlocks/Active_Lava.cs
+++ MCForge 2.0/World/Blocks/Physics/DefaultPhysicBlocks/Active_Lava.cs
@@ -13,6 +13,9 @@ or implied. See the Licenses for the specific language governing
 permissions and limitations under the Licenses.
 */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using MCForge.Utils;
 
 namespace MCForge.World.Physics
 {
@@ -35,10 +38,93 @@ namespace MCForge.World.Physics
         {
 
         }
-        public Active_Lava() {}
+        public Active_Lava() { }
         public override void Tick(Level l)
         {
-            throw new NotImplementedException();
+            /* .....-----===== Explosion =====-----..... */
+            /*List<Vector3S> tweet = new List<Vector3S>();
+            Random r = new Random();
+            int size = 10;
+            for (ushort xx = (ushort)(X - (size + 1)); xx <= (ushort)(X + (size + 1)); xx++)
+                for (ushort zz = (ushort)(Z - (size + 1)); zz <= (ushort)(Z + (size + 1)); zz++)
+                    for (ushort yy = (ushort)(Y - (size + 1)); yy <= (ushort)(Y + (size + 1)); yy++)
+                    {
+                        if (r.Next(1, 11) <= 4)
+                        {
+                            l.BlockChange(xx, zz, yy, Block.BlockList.LAVA);
+                            tweet.Add(new Vector3S(xx, zz, yy));
+                        }
+                        else if (r.Next(1, 11) <= 8)
+                        {
+                            l.BlockChange(xx, zz, yy, Block.BlockList.AIR);
+                        }
+                    }
+            for (ushort xx = (ushort)(X - (size + 2)); xx <= (ushort)(X + (size + 2)); xx++)
+                for (ushort zz = (ushort)(Z - (size + 2)); zz <= (ushort)(Z + (size + 2)); zz++)
+                    for (ushort yy = (ushort)(Y - (size + 2)); yy <= (ushort)(Y + (size + 2)); yy++)
+                    {
+                        if (r.Next(1, 11) <= 4)
+                        {
+                            l.BlockChange(xx, zz, yy, Block.BlockList.LAVA);
+                            tweet.Add(new Vector3S(xx, zz, yy));
+                        }
+                        else if (r.Next(1, 11) <= 8)
+                        {
+                            l.BlockChange(xx, zz, yy, Block.BlockList.AIR);
+                        }
+                    }
+
+                        for (ushort xx = (ushort)(X - (size + 3)); xx <= (ushort)(X + (size + 3)); xx++)
+                for (ushort zz = (ushort)(Z - (size + 3)); zz <= (ushort)(Z + (size + 3)); zz++)
+                    for (ushort yy = (ushort)(Y - (size + 3)); yy <= (ushort)(Y + (size + 3)); yy++)
+                    {
+                        if (r.Next(1, 11) <= 4)
+                        {
+                            l.BlockChange(xx, zz, yy, Block.BlockList.LAVA);
+                            tweet.Add(new Vector3S(xx, zz, yy));
+                        }
+                        else if (r.Next(1, 11) <= 8)
+                        {
+                            l.BlockChange(xx, zz, yy, Block.BlockList.AIR);
+                        }
+                    }
+                        //Clean up
+                        foreach (Vector3S lol in tweet)
+                        {
+                            if (l.GetBlock(lol) == Block.BlockList.LAVA)
+                            {
+                                l.BlockChange(lol, Block.BlockList.AIR);
+                            }
+                        }*/
+            
+            /* .....-----===== Fireworks =====-----..... */
+
+            /*Random r = new Random();
+            List<Vector3S> tweet = new List<Vector3S>();
+            int storedRand1 = r.Next(21, 36);
+            int storedRand2 = r.Next(21, 36);
+            int size = 15;
+            for (ushort xx = (ushort)(X - (size + 1)); xx <= (ushort)(X + (size + 1)); xx++)
+                for (ushort zz = (ushort)(Z - (size + 1)); zz <= (ushort)(Z + (size + 1)); zz++)
+                    for (ushort yy = (ushort)(Y - (size + 1)); yy <= (ushort)(Y + (size + 1)); yy++)
+                    {
+                        if (r.Next(1, 40) < 2)
+                        {
+                            byte b = (byte)(r.Next(Math.Min(storedRand1, storedRand2), Math.Max(storedRand1, storedRand2)));
+                            l.BlockChange(xx, zz, yy, b);
+                            tweet.Add(new Vector3S(xx, zz, yy));
+                        }
+                    }
+
+            //Clean up
+            foreach (Vector3S lol in tweet)
+            {
+                if (l.GetBlock(lol) == Block.BlockList.LAVA)
+                {
+                    l.BlockChange(lol, Block.BlockList.AIR);
+                }
+            }*/
+
         }
     }
 }
