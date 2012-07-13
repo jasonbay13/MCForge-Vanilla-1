293366954c39237f4c6d58628e43c09fe02d303e
GameMakerGm <gamemakergm@safaree.org>
2012-07-13 00:19:13 -0600

    For 7imey

--- MCForge 2.0/World/Blocks/DefaultBlocks/Dirt.cs
+++ MCForge 2.0/World/Blocks/DefaultBlocks/Dirt.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright 2011 MCForge
 Dual-licensed under the Educational Community License, Version 2.0 and
 the GNU General Public License, Version 3 (the "Licenses"); you may
@@ -11,15 +11,13 @@ software distributed under the Licenses are distributed on an "AS IS"
 BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied. See the Licenses for the specific language governing
 permissions and limitations under the Licenses.
-*/
+ */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using MCForge.Utils;
 
-namespace MCForge.World.Blocks
+namespace MCForge.World.Physics
 {
-    public class Dirt : Block
+    public class Dirt : PhysicsBlock
     {
         public override string Name
         {
@@ -32,6 +30,29 @@ namespace MCForge.World.Blocks
         public override byte Permission
         {
             get { return 0; }
+        }
+        public Dirt(int x, int z, int y)
+            : base(x, z, y)
+        {
+        }
+        public Dirt() { }
+
+        public override object Clone()
+        {
+            Dirt g = new Dirt();
+            g.X = X;
+            g.Y = Y;
+            g.Z = Z;
+            return g;
+        }
+
+        public override void Tick(Level l)
+        {
+            if (l.GetBlock(X, Z, Y + 1) == Block.BlockList.AIR)
+            {
+                l.BlockChange((ushort)X, (ushort)Z, (ushort)Y, Block.BlockList.GRASS);
+                Remove(l);
+            }
         }
     }
 }
