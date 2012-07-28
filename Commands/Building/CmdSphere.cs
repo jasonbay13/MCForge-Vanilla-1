c388d209c6d72c6960d7e97996808e53209192f8
Aaron Lawson <cripplecowboycowoutfit@yahoo.com>
2012-07-28 16:27:39 -0500

    Changed /ball to /spheroid

--- Commands/Building/CmdSphere.cs
+++ Commands/Building/CmdSphere.cs
@@ -7,41 +7,53 @@ using MCForge.Entity;
 using MCForge.World;
 using MCForge.Utils;
 
-namespace MCForge.Commands.Building {
-    public class CmdBall :ICommand {
-        public string Name {
-            get {  return "Ball"; }
+namespace MCForge.Commands.Building
+{
+    public class CmdSpheroid : ICommand
+    {
+        public string Name
+        {
+            get { return "Spheroid"; }
         }
 
-        public CommandTypes Type {
+        public CommandTypes Type
+        {
             get { return CommandTypes.Building; }
         }
 
-        public string Author {
+        public string Author
+        {
             get { return "ninedrafted"; }
         }
 
-        public int Version {
+        public int Version
+        {
             get { return 1; }
         }
 
-        public string CUD {
+        public string CUD
+        {
             get { return ""; }
         }
 
-        public byte Permission {
+        public byte Permission
+        {
             get { return 0; }
         }
 
-        public void Use(Player p, string[] args) {
+        public void Use(Player p, string[] args)
+        {
             int rx = 10;
-            if (args.Length > 0) {
-                try {
+            if (args.Length > 0)
+            {
+                try
+                {
                     rx = Int32.Parse(args[0]);
                     if (args.Length > 1) p.ExtraData["BallType"] = args[1];
                     else p.ExtraData["BallType"] = null;
                 }
-                catch {
+                catch
+                {
                     p.ExtraData["BallType"] = args[0];
                 }
             }
@@ -50,7 +62,8 @@ namespace MCForge.Commands.Building {
             p.SendMessage("Define center");
         }
 
-        void OnPlayerBlockChange_Normal(Player sender, API.Events.BlockChangeEventArgs args) {
+        void OnPlayerBlockChange_Normal(Player sender, API.Events.BlockChangeEventArgs args)
+        {
             sender.OnPlayerBlockChange.Normal -= OnPlayerBlockChange_Normal;
             int rx = (int)sender.ExtraData["BallRadius"];
             int count = 0;
@@ -59,8 +72,10 @@ namespace MCForge.Commands.Building {
                 blocks = (new Vector3S(args.X, args.Z, args.Y)).GetNearBlocksHollow(rx, rx, rx);
             else
                 blocks = (new Vector3S(args.X, args.Z, args.Y)).GetNearBlocks(rx, rx, rx);
-            foreach (Vector3S v in blocks) {
-                if (v.x > 0 && v.z > 0 && v.y > 0 && v.x < sender.Level.Size.x && v.z < sender.Level.Size.z && v.y < sender.Level.Size.y) {
+            foreach (Vector3S v in blocks)
+            {
+                if (v.x > 0 && v.z > 0 && v.y > 0 && v.x < sender.Level.Size.x && v.z < sender.Level.Size.z && v.y < sender.Level.Size.y)
+                {
                     sender.Level.BlockChange(v, args.Holding, sender);
                     count++;
                 }
@@ -68,17 +83,19 @@ namespace MCForge.Commands.Building {
             sender.SendMessage(count + " Blocks");
         }
 
-        public void Help(Player p) {
-            p.SendMessage("/ball - uses radius 10 and type fill");
-            p.SendMessage("/ball [radius] - uses given radius and type fill");
-            p.SendMessage("/ball [type] - uses radius 10 and given type");
-            p.SendMessage("/ball [radius] [type] - uses given radius and given type");
-            p.SendMessage("Creates a ball");
+        public void Help(Player p)
+        {
+            p.SendMessage("/spheroid - uses radius 10 and type fill");
+            p.SendMessage("/spheroid [radius] - uses given radius and type fill");
+            p.SendMessage("/spheroid [type] - uses radius 10 and given type");
+            p.SendMessage("/spheroid [radius] [type] - uses given radius and given type");
+            p.SendMessage("Creates a sphere");
             p.SendMessage("Accepted types are: fill, hollow");
         }
 
-        public void Initialize() {
-            Command.AddReference(this, "Ball");
+        public void Initialize()
+        {
+            Command.AddReference(this, "spheroid");
         }
     }
 }
